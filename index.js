a328922738bf7a1d4ff1ccf8b182c97ce640e7d4
masazevedo <marcello.azevedo@spassu.com.br>
2020-11-29 11:45:22 -0300

    react dom com bug

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const rootElement = document.getElementById('app');
 
-const myElement = document.createElement('h1');
-myElement.className = "orange";
-myElement.innerText = 'Olá meu mundo laranja';
+// const myElement = document.createElement('h1');
+// myElement.className = "orange";
+// myElement.innerText = 'Olá meu mundo laranja';
 
-rootElement.appendChild(myElement);
+const myReactElement = React.createElement('h1', 
+     { className = 'orange'},
+     'Olá meu mundo laranja.');
+
+// rootElement.appendChild(myElement);
+
+ReactDOM.render(
+    myReactElement,
+    document.getElementById('app')
+);
